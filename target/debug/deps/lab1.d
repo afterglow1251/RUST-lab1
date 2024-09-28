16a47bd81925d833403bf9db1fc810550cf907b8
Merciless <yurii.afterglow@gmail.com>
2024-09-28 13:50:09 +0300

    lab 1

---
+++ target/debug/deps/lab1.d
@@ -0,0 +1,5 @@
+C:\Users\User\RustroverProjects\lab1\target\debug\deps\lab1.exe: src/main.rs
+
+C:\Users\User\RustroverProjects\lab1\target\debug\deps\lab1.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/lab1.d
@@ -0,0 +1,1 @@
+C:\Users\User\RustroverProjects\lab1\target\debug\lab1.exe: C:\Users\User\RustroverProjects\lab1\src\main.rs
